c48a41c3006e3ef738e81188a1e8419574c5c274
singhanisha <anishas2218@gmail.com>
2024-03-02 18:38:50 +0530

    my-app

---
+++ flyway/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import FlightRouter from './router/FlightRouter';
+
+function App() {
+  return (
+    <div className="App">
+ 
+      <FlightRouter/>
+
+    </div>
+  );
+}
+
+export default App;
